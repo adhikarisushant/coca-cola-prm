1a8778771a25860a99b0a2c39f89ce31a4420cdd
adhikarisushant <sushant.adhikari.619@gmail.com>
2024-01-15 22:33:06 +0545

    server error handler

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import express, { Request, Response, NextFunction } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -28,3 +29,13 @@ app.get("/test", (req: Request, res: Response, next: NextFunction) => {
     message: "API is working",
   });
 });
+
+// unknown api route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use(ErrorMiddleware);
---
+++ server/middleware/error.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+
+export const ErrorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  // wrong jwt error
+  if (err.code === "JsonWebTokenError") {
+    const message = `Json web token is invalid, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // jwt expired error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json web token is expired, try again`;
+
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({ success: false, message: err.message });
+};
